b1140467c5d36e1024593d1ccd5f815b2bcede69
affan055 <8277262677ali@gmail.com>
2026-07-06 16:20:38 +0530

    added

--- apartment_register/todo_app/views.py
+++ apartment_register/todo_app/views.py
@@ -12,7 +12,7 @@ def login_view(request):
             user = form.get_user()
             login(request, user)
             messages.success(request, 'You are now logged in.')
-            return redirect('todo_page')
+            return redirect('/dashboard/')
     else:
         form = AuthenticationForm()
 
